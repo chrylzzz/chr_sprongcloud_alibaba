9d42d99f33679a1c3b1738b968a48f05ec260e77
chryl <cyl5yy412@gmail.com>
2020-06-22 10:16:12 +0800

    sentinel 和feign整合,以及feign 容错类 调用出错信息的查看

--- shop-order/src/main/java/com/chryl/config/CommonFilterConfig.java
+++ shop-order/src/main/java/com/chryl/config/CommonFilterConfig.java
@@ -10,8 +10,8 @@
 // * 因此链路限流不生效,但是1.7.0开始,可以进行手动的关闭该限制,
 // * 开放链路限流,这里进行手动开启
 // * springcloud alibaba 2.1.1版本之后,可以直接配置打开链路限流,更为简单,
-// * spring.cloud.sentinel.web-context-unifyfalse即可关闭
-// * 但是习时springcloud alibaba 2.1.1还未发布,所以只能手动关闭
+// * spring.cloud.sentinel.web-context-unify=false即可关闭
+// * 但是习时springcloud alibaba 2.1.2还未发布,当前版本为springcloud alibaba 2.1.1,所以只能手动关闭
 // * Created by Chr.yl on 2020/6/20.
 // *
 // * @author Chr.yl
